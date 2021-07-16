6fc4060af9ab549fb3014bc334921b03404a2a03
giselledunine <sophia.hmamouche@outlook.fr>
2021-07-16 10:38:43 +0200

    responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -37,6 +37,16 @@
                </template>
 
                <v-list class="menu">
+                 <v-list-item @click="toDashboard">
+                   <v-list-item-icon>
+                     <v-icon>
+                       mdi-account
+                     </v-icon>
+                   </v-list-item-icon>
+                   <v-list-item-content>
+                     <v-list-item-title>Dashboard</v-list-item-title>
+                   </v-list-item-content>
+                 </v-list-item>
                  <v-list-item>
                    <v-btn @click="signOut" class="text">
                      <v-icon left>
